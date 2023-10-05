1cd24cbcb33a3ed274c3e17b0d5143dadeec4424
Mariem <mariem.ehab@ieee.org>
2023-10-05 16:05:27 -0700

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,10 @@ if __name__ == "__main__":
     args = sys.argv[1:]
 
     if len(args) > 0:
-        print("{} arguments:".format(len(args)))
+        if len(args) == 1:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(len(args)))
         for i in range(len(args)):
             print("{}: {}".format(i + 1, args[i]))
     else:
