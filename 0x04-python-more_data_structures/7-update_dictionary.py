650b79105373422da527487e29265630ce8a104c
Mariem <mariem.ehab@ieee.org>
2023-10-11 04:32:42 -0700

    7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    new_dict = dict()
-    for i in a_dictionary.keys():
-        new_dict[i] = a_dictionary.get(i)
+    new_dict = a_dictionary
 
     new_dict[key] = value
     return new_dict
