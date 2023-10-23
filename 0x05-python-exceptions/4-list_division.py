dfa1c5665281587eb5583a79797ae9c25ece24c2
Mariem <mariem.ehab@ieee.org>
2023-10-23 07:37:56 -0700

    4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -14,7 +14,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         except IndexError:
             print("out of range")
-            break
+            result = 0
         finally:
             new_list.append(result)
 
