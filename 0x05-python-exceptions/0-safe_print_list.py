8555613fd1cc5845a5cf1f316ec63db5025c1eee
Mariem Ehab <mariem.ehab@ieee.org>
2023-10-23 13:47:24 +0000

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-	l = 0
+	length = 0
 	for e in range(x):
 		try:
 			print("{}".format(my_list[e]), end= "")
-			l += 1
+			length += 1
 		except Exception:
 			break
 	print("")
-	return l
+	return length
