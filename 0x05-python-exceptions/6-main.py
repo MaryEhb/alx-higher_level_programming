eb456e82c57696a79a0534b639549f6b8bc0342b
Mariem <mariem.ehab@ieee.org>
2023-10-23 07:49:21 -0700

    6

--- 0x05-python-exceptions/6-main.py
+++ 0x05-python-exceptions/6-main.py
@@ -1,4 +1,5 @@
-se_exception_msg = __import__('6-raise_exception_msg').raise_exception_msg
+#!/usr/bin/python3
+raise_exception_msg = __import__('6-raise_exception_msg').raise_exception_msg
 
 try:
     raise_exception_msg("C is fun")
