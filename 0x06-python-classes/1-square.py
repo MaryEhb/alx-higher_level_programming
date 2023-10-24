f03abd0149693c259819e163fbaa773c7b172bdd
Mariem <mariem.ehab@ieee.org>
2023-10-24 04:54:05 -0700

    2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define Square class"""
+
+
+class Square:
+    """square class
+    Attributes:
+    size: size
+    """
+    def __init__(self, size):
+        self.size = size
