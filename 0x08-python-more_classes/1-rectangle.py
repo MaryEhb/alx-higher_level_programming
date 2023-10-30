2a6d71eba4df1475f5c67401d5b04ab12c74eb65
Mariem <mariem.ehab@ieee.org>
2023-10-30 09:34:09 -0700

    addding @property and @prop.setter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,9 +9,11 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
+    @property
     def width(self):
         return self.__width
 
+    @width.setter
     def width(self, value):
 
         if not isinstance(value, int):
@@ -21,9 +23,11 @@ class Rectangle:
 
         self.__width = value
 
+    @property
     def height(self):
         return self.__height
 
+    @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
