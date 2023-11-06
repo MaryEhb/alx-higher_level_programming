0275aaa14e2a9a18ae834eff402a073ebe07ffb7
Mariem <mariem.ehab@ieee.org>
2023-11-06 14:48:03 -0800

    Task 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """2. Exact same object"""
 
+
 def is_same_class(obj, a_class):
     """ returns True if the object is exactly an instance of
     the specified class ; otherwise False."""
