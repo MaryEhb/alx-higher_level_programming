7398af82ea5efa338bfe9af5475ca84e949ac6e6
Mariem <mariem.ehab@ieee.org>
2023-11-07 12:57:45 -0800

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""1. Write to a file"""
 
 
 def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8) and returns the number
+    of characters"""
     with open(filename, 'w', encoding="UTF8") as file:
         file.write(text)
     return len(text)
-
