bfde6c53a365ff2bb23960997398750757b118ce
Mariem <mariem.ehab@ieee.org>
2023-11-07 14:19:11 -0800

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""9. Student to JSON"""
+
+
+class Student:
+    """student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__.copy()
+
+    def to_json(self, attrs=None):
+        dic = self.__dict__.copy()
+
+        if isinstance(attrs, list):
+            newdic = {}
+            for item in attrs:
+                if item in dic:
+                    newdic[item] = dic[item]
+            return newdic
+
+        else:
+            return dic
