43ce1930597956f34cf0927c5be67270024778fd
Mariem <mariem.ehab@ieee.org>
2023-11-13 07:18:43 -0800

    Square doc

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,6 +6,8 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """Square class inhiret from Rect"""
+
     def __init__(self, size, x=0, y=0, id=None):
         """Initialization of square obj"""
         super().__init__(size, size, x, y, id)
