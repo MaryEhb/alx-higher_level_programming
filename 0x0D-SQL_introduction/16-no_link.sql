a7eb5f40075afe9df4a18dc2f80f32c245afc87f
Mariem <mariem.ehab@ieee.org>
2023-12-12 12:51:45 -0800

    16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 --  lists all records of the table second_table but no rows without a name value
-SELECT * FROM second_table WHERE NOT name = NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE NOT name = NULL ORDER BY score DESC;
