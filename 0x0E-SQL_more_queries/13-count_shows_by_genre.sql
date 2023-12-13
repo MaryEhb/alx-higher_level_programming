34badc386b5c206fa85547cd8a84bcbf46866f8f
Mariem <mariem.ehab@ieee.org>
2023-12-13 13:26:55 -0800

    13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- Each record should display: <TV Show genre> - <Number of shows linked to this genre>
+SELECT tv_genres.name As genre, COUNT(*) A number_of_shows
+FROM tv_genres JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_of_shows DESC;
