6ed98bdac4500444f5e68a65fd35b8ebb94a57cf
Mariem <mariem.ehab@ieee.org>
2023-12-13 13:56:38 -0800

    15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
 -- lists all Comedy shows in the database hbtn_0d_tvshows
 SELECT tv_shows.title FROM tv_shows
-JOIN tv_shows_genre ON tv_shows_genre.show_id =  tv_shows.id
 JOIN tv_genre ON tv_shows_genre.genre_id = tv_genres.id
+JOIN tv_shows_genre ON tv_shows_genre.show_id =  tv_shows.id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title asc;
