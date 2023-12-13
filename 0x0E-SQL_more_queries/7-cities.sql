15ab6fd7f5f5da5ff844956cdf699e1417532242
Mariem <mariem.ehab@ieee.org>
2023-12-13 11:09:00 -0800

    7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,6 +4,6 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL,
-	state_id INT,
+	state_id INT NOT NULL,
 	FOREIGN KEY(state_id) REFERENCES states(id)
 );
