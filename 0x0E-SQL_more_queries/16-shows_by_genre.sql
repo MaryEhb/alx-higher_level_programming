d24bc1c18e47c25ea9f8ae99bacb2bdd3a025ce2
Mariem <mariem.ehab@ieee.org>
2023-12-13 14:02:49 -0800

    16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all shows, and all genres linked to that show
-select tv_shows.title,  tv_genres.name from tv_shows
-LEFT join tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-LEFT join tv_genres ON tv_show_genres.genre_id = tv_genres.id
+SELECT tv_shows.title,  tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title, tv_genres.name;
