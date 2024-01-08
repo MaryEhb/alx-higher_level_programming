bebb94443028f956b8468ae86eba2bf97b39e508
Mariem Ehab <mariem.ehab@ieee.org>
2024-01-08 09:40:21 -0800

    Task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function fact(a) {
+  if (a > 1) {
+    return a * fact(a - 1);
+  }
+  return 1;
+}
+
+if (!isNaN(process.argv[2])) {
+  console.log(fact(parseInt(process.argv[2])));
+} else {
+  console.log(1);
+}
