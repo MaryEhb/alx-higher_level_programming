71d304ac6078b267c184b3af8434451d0365216c
Mariem <mariem.ehab@ieee.org>
2024-01-11 14:29:27 -0800

    Task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+script that lists the first State objects from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True)
+
+    session = sessionmaker(bind=engine)
+    session = session()
+
+    Base.metadata.create_all(engine)
+
+    state = session.query(State).filter_by(id='2').first()
+    state.name = "New Mexico"
+
+    session.commit()
+    session.close()
