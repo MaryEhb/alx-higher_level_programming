e2ff06abb17f0f038a9781a1dad52164bac28ee8
Mariem Ehab <mariem.ehab@ieee.org>
2024-01-26 13:56:40 -0800

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     try:
         with request.urlopen(sys.argv[1]) as res:
             print(res.read().decode('utf-8'))
-    except error.HTTPError:
-        print("Error code: {}".format(res.status()))
+    except error.HTTPError as err:
+        print("Error code: {}".format(err.code()))
