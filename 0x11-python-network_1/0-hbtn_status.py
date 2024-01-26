cfa257926a5f9bf13c233acaf308bdf3c82ec2bb
Mariem Ehab <mariem.ehab@ieee.org>
2024-01-26 13:10:34 -0800

    Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
     body = res.read()
-    print("Body response:\n\
-    \t- type: {}\n\
-    \t- content: {}\n\
-    \t- utf8 content: {}".format(type(body), body, body.decode('utf-8')))
+    str = "Body response:\n\t- type: {}".format(type(body))
+    str += "\n\t- content: {}".format(body)
+    str += "\n\t- utf8 content: {}".format(body.decode('utf-8'))
+    print(str)
