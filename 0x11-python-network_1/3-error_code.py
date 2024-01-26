5422d45cdf531e3c48d6c28cfb05bc61b77ac961
Mariem Ehab <mariem.ehab@ieee.org>
2024-01-26 13:54:55 -0800

    task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""3. Error code #0"""
+
+from urllib import request, error
+import sys
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res.read().decode('utf-8'))
+    except error.HTTPError:
+        print("Error code: {}".format(res.status()))
