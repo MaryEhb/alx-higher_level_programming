5afa8a4664b020d2b36ce2205b8bade56289f47e
Mariem Ehab <mariem.ehab@ieee.org>
2024-01-26 13:16:57 -0800

    Task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 1. Response header value #0
+
+import urllib.request
+import sys
+
+with urllib.request.urlopen(sys.argv[1]) as res:
+    result = res.getheader('Request-Id')
+    print(result)
