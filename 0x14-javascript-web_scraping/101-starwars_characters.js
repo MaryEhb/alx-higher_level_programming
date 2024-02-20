a606ba8b46ce265f8f4bde562271c960d2515337
Your Name <you@example.com>
2024-02-20 11:52:36 -0800

    Task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,20 +2,31 @@
 /* prints all characters of a Star Wars movie */
 
 const request = require('request');
+
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+// Function to fetch and print character names sequentially
+function fetchAndPrintCharacter (urlArray, index) {
+  if (index >= urlArray.length) {
+    return; // Exit when all characters have been printed
+  }
+
+  const characterUrl = urlArray[index];
+  request.get(characterUrl, (err, res, body) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(JSON.parse(body).name);
+      fetchAndPrintCharacter(urlArray, index + 1); // Fetch next character recursively
+    }
+  });
+}
+
 request.get(url, (err, res, body) => {
   if (err) {
     console.error(err);
   } else {
     const characters = JSON.parse(body).characters;
-    for (const url of characters) {
-      request.get(url, (err, res, body) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+    fetchAndPrintCharacter(characters, 0); // Start fetching characters sequentially
   }
 });
