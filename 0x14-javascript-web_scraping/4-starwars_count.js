d7265dd51eba62e49ff06423c08ac8b768bcf48f
Your Name <you@example.com>
2024-02-20 09:46:55 -0800

    Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,26 +1,25 @@
 #!/usr/bin/node
-/* prints the number of Star Wars films a character appears in. */
 
 const request = require('request');
+const starWarsUri = process.argv[2];
+let times = 0;
 
-try {
-  const url = process.argv[2];
+request(starWarsUri, function (_err, _res, body) {
+  body = JSON.parse(body).results;
 
-  const characterURL = 'https://swapi-api.alx-tools.com/api/people/18/';
-  request.get(url, (err, res, body) => {
-    if (err) {
-      console.error(err);
-    } else {
-      let count = 0;
-      const result = JSON.parse(body).results;
-      for (const film of result) {
-        if (film.characters.includes(characterURL)) {
-          count++;
-        }
+  for (let i = 0; i < body.length; ++i) {
+    const filmId = body[i].url.split('/')[5];
+    const characters = body[i].characters;
+
+    for (let j = 0; j < characters.length; ++j) {
+      const character = characters[j];
+      const characterId = character.split('/')[5];
+
+      if (characterId === '18') {
+        times += 1;
       }
-      console.log(count);
     }
-  });
-} catch (e) {
-  console.error(e);
-}
+  }
+
+  console.log(times);
+});
